d1f2f9a0f43de39fba99336c361451502a823c35
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-29 03:13:01 +0530

    refactor: remove duplication in layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
+import { Flex } from "@chakra-ui/react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
 const Layout = () => {
   return (
-    <>
+    <Flex 
+      direction="column" 
+      minH="100vh"
+    >
       <Navbar />
       <Outlet />
       <Footer />
-    </>
+    </Flex>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,8 @@
-import { Flex } from "@chakra-ui/react";
-import NavBar from "../components/Navbar/Navbar"; // Assuming NavBar exists
-import Footer from "../components/Footer/Footer"; // Footer from the earlier implementation
 import HomeMain from "../components/HomeMain/HomeMain";
 
 function Home() {
   return (
-    <Flex 
-      direction="column" 
-      minH="100vh"
-    >
-      <NavBar />
-      <HomeMain />
-      <Footer />
-    </Flex>
+    <HomeMain />
   );
 }
 
