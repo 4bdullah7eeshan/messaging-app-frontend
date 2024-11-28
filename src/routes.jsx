255d5e9c235880d34fbdedc9fcd6e1dd2674ad02
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-29 00:12:26 +0530

    feat: create a layout, wip routes

---
+++ src/routes.jsx
@@ -0,0 +1,40 @@
+import App from "./App";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import ErrorPage from "./pages/ErrorPage";
+import Layout from "./components/Layout/Layout";
+import Product from "./pages/Product";
+import About from "./pages/About";
+import { element } from "prop-types";
+
+const routes = [
+  {
+    path: "/",
+    element: <Layout />, // Use the layout here
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "", // This will match the root path
+        element: <App />, // Render App here if needed
+      },
+      {
+        path: "products",
+        element: <Products />, // Render Products page here
+      },
+      {
+        path: "products/:productId",
+        element: <Product />,
+      },
+      {
+        path: "cart",
+        element: <Cart />, // Render Cart page here
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+];
+
+export default routes;
