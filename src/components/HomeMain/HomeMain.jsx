3f5d7f1db592bcacc19a09605ecedf14c9c1c682
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-30 15:39:31 +0530

    refactor: clean home main

--- src/components/HomeMain/HomeMain.jsx
+++ src/components/HomeMain/HomeMain.jsx
@@ -1,11 +1,10 @@
 function HomeMain() {
-  return (
-    <main className="flex flex-col items-center justify-center flex-grow text-center bg-gray-50 dark:bg-gray-900">
-      <h1 className="text-4xl font-bold text-gray-700 dark:text-gray-200">
-        Welcome
-      </h1>
-    </main>
-  );
-}
+    return (
+        <main>
 
+        </main>       
+    );
+}
+  
 export default HomeMain;
+  
